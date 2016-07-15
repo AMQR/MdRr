ceff0d11b2876332659dbd0d9270d3da284ab415
amqr <icoollike@sina.com>
2016-07-15 16:12:13 +0800

    init commit

---
+++ app/src/main/java/com/am/mdrr/MainActivity.java
@@ -0,0 +1,19 @@
+package com.am.mdrr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
